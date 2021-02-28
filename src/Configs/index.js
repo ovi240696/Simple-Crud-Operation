cd5bfb15ef0a553db29371c45a306044b8a26111
Owais <ovi240696@gmail.com>
2021-02-28 18:24:20 +0500

    folders created and config file created

---
+++ src/Configs/index.js
@@ -0,0 +1,7 @@
+export default {
+    base_url: process.env.REACT_APP_BASE_URL,
+    client_id: process.env.REACT_APP_CLIENT_ID,
+    client_secret: process.env.REACT_APP_CLIENT_SECRET,
+    public_url: process.env.REACT_APP_PUBLIC_URL,
+    web_url: process.env.REACT_APP_WEB_URL,
+};
